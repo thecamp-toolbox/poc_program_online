8dd0b660dfb9b4c1a066145c1de526dea34060f5
Julien Belin <julienbelin002@gmail.com>
2018-12-11 18:12:02 +0100

    Uniformiser le rendu des docs dans le snippet affiché sur la page programme

--- site/snippets/docs-card.php
+++ site/snippets/docs-card.php
@@ -11,20 +11,44 @@
 	        <?php endif ?>
         </div>
         <div class="card-body">
-            <ul class="list-unstyled list-spacing-sm">
-            	<?php foreach ($docs as $doc) : ?>
-                    <li>
-                        <i class="fas fa-file text-muted mr-1"></i>
-                        <a href="<?= $doc->url() ?>" download>
-                        	<?php if ($doc->niceName() != '') {
-                        		echo $doc->niceName();
-                        	} else {
-                        		echo $doc->filename();
-                        	} ?>
-                        </a>
-                    </li>
-                <?php endforeach ?>
-            </ul>
+              <table class="table table-borderless table-hover align-items-center">
+                  <tbody>
+                    <?php foreach ($docs as $doc) : ?>
+                        <tr class="bg-white">
+                            <th scope="row">
+                                <a class="media align-items-center" href="<?= $doc->url() ?>" download>
+                                  <?php $type = $doc->type() ?>
+                                  <?php if ($type == 'image') {
+                                    $type = 'image';
+                                  } else if ($type == 'archive') {
+                                    $type = 'folder';
+                                  } else if ($type == 'video') {
+                                    $type = 'film';
+                                  } else {
+                                    $type = 'doc';
+                                  } ?>
+                                    <img alt="Image" src="<?= $site->url() ?>/assets/images/<?= $type ?>.jpg" class="avatar rounded avatar-sm" />
+                                    <div class="media-body">
+                                        <span class="text-muted mb-0">
+                                          <?php if($doc->niceName() != '') {
+                                            echo $doc->niceName();
+                                          } else {
+                                            echo $doc->fileName();
+                                          } ?>
+                                          <?= $type ?>
+                                        </span>
+                                    </div>
+                                </a>
+                            </th>
+
+                            <td>
+                                <a class="media align-items-center" href="<?= $doc->url() ?>" download><i class="fas fa-download"></i></a>
+                            </td>
+                        </tr>
+                        <tr class="table-divider"></tr>
+                    <?php endforeach ?>
+                  </tbody>
+              </table>
         </div>
     </div>
     <!--end of card-->
