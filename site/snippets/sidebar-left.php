3bac1340e8c41ccddd3a06b63e401333510db97b
Julien Belin <julienbelin002@gmail.com>
2019-01-17 16:38:10 +0100

    Adding dynamic tools list on the left sidebar

--- site/snippets/sidebar-left.php
+++ site/snippets/sidebar-left.php
@@ -1,33 +1,34 @@
-<nav class="col-md-2 col-left">
+<?php $tools = $prog->toolbox();?>
+
+<nav class="col-md-2 col-left col-sidebar">
   <div class="sidebar-sticky">
   	<div class="row">
-  		<?php $phases = $prog->children()->filterBy('template', 'phase') ?>
-  		<?php if ($phases != '') : ?>
-  			<?php foreach ($phases->invisible() as $phase) : ?>
-  				<?= snippet('phase-menu', array('phase'=>$phase, 'prog'=>$prog)) ?>
-  			<?php endforeach ?>
-  			<?php foreach ($phases->visible() as $phase) : ?>
-  				<?= snippet('phase-menu', array('phase'=>$phase, 'prog'=>$prog)) ?>
-  			<?php endforeach ?>
-  		<?php endif ?>
+      <div class="sidebar-phase">
+        <?php $phases = $prog->children()->filterBy('template', 'phase') ?>
+    		<?php if ($phases != '') : ?>
+    			<?php foreach ($phases->invisible() as $phase) : ?>
+    				<?= snippet('phase-menu', array('phase'=>$phase, 'prog'=>$prog)) ?>
+    			<?php endforeach ?>
+    			<?php foreach ($phases->visible() as $phase) : ?>
+    				<?= snippet('phase-menu', array('phase'=>$phase, 'prog'=>$prog)) ?>
+    			<?php endforeach ?>
+    		<?php endif ?>
+      </div>
+      <? if(!$tools->empty()):?>
+    		<!-- link to tools -->
+    		<div class="sidebar-tools list-group list-group-flush w-100" id="listz" role="tablist">
+    			<span class="list-group-item tools">
+    				<ul class="list-inline text-small d-inline-block">
+              <?php foreach ($tools->toStructure() as $tool) : ?>
+                <a target="_blank" href="<?= $tool->url() ?>">
+                  <li class="list-inline-item"><i class="fab fa-<?= $tool->icon() ?> mr-1"></i></li>
+                </a>
+              <?php endforeach ?>
+              </ul>
+          </span>
+    		</div>
+      <? endif; ?>
 
-  		<!-- link to tools -->
-		<div class="list-group list-group-flush w-100" id="listz" role="tablist">
-			<span class="list-group-item tools">
-				<ul class="list-inline text-small d-inline-block">
-                    <a href="">
-                    	<li class="list-inline-item"><i class="fab fa-dropbox mr-1"></i></li>
-                    </a>
-                    <a href="">
-                        <li class="list-inline-item"><i class="fas fa-book mr-1"></i></li>
-                    </a>
-                    <a href="">
-                        <li class="list-inline-item"><i class="fab fa-slack mr-1"></i></li>
-                    </a>
-                </ul>
-			    
-            </span>
-		</div>					
-	</div><!-- end row -->
+	 </div><!-- end row -->
   </div>
 </nav><!-- end col -->
